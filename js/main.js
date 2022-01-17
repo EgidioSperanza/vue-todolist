dd9be1351388f7079a83b2eec4feb9344315a1f4
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-01-17 15:46:16 +0100

    indentazione migliorata

--- js/main.js
+++ js/main.js
@@ -1,38 +1,37 @@
 new Vue({
-    el: "#app",
-    data:{
-        todoList:[
-            {
-                text: "Fare la spesa",
-                isDone: false
-            },
-            {
-                text: "Telefonare il meccanico per riparazione auto",
-                isDone: false
-            },
-            {
-                text: "Pagare affitto",
-                isDone: false
-            }
-        ],
-        newTodo:"",
+  el: "#app",
+  data: {
+    todoList: [
+      {
+        text: "Fare la spesa",
+        isDone: false,
+      },
+      {
+        text: "Telefonare il meccanico per riparazione auto",
+        isDone: false,
+      },
+      {
+        text: "Pagare affitto",
+        isDone: false,
+      },
+    ],
+    newTodo: "",
+  },
+  methods: {
+    getDone(index) {
+      if (this.todoList[index].isDone) return "completed";
+      else return "incomplete";
     },
-    methods:{
-        getDone(index){
-            if (this.todoList[index].isDone)
-                return "completed";
-            else return "incomplete";
-        },
-        setDone(e){
-            e.isDone ? e.isDone=false : e.isDone=true;
-        },
-        deleteTodo(index){
-            this.todoList.splice(index, 1);
-        },
-        addTodo(e){
-            this.newTodo=e;
-            this.todoList.push({text:e, isDone:false});
-            this.newTodo="";
-        }
-    }
-});  
+    setDone(e) {
+      e.isDone ? (e.isDone = false) : (e.isDone = true);
+    },
+    deleteTodo(index) {
+      this.todoList.splice(index, 1);
+    },
+    addTodo(e) {
+      this.newTodo = e;
+      this.todoList.push({ text: e, isDone: false });
+      this.newTodo = "";
+    },
+  },
+});
